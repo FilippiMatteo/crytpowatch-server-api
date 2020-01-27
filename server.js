c29d6544070bc787abe0fce9f0b829fee0ee67a1
FilippiMatteo <58559797+FilippiMatteo@users.noreply.github.com>
2020-01-27 17:55:25 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ var app = express();
 //app.use(cors({origin: '*'}));
 
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.json());
+//app.use(express.urlencoded({extended: false}));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', (req, res, next) => {
     console.log("test");
